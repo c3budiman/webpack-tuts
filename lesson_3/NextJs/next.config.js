1b29c8d21e0a8792abe5f2ae47e34cf92bf15afe
Cecep Budiman <c3budiman@gmail.com>
2023-03-26 21:10:12 +0700

    webpack in next js

--- lesson_3/NextJs/next.config.js
+++ lesson_3/NextJs/next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
+// https://nextjs.org/docs/api-reference/next.config.js/custom-webpack-config
+
+// Cara Manual :
+// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+// const nextConfig = {
+//   reactStrictMode: true,
+//   webpack: (config, options) => {
+//     config.plugins.push(new BundleAnalyzerPlugin())
+//     return config
+//   },
+// }
+
 const nextConfig = {
   reactStrictMode: true,
 }
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: true,
+})
 
-module.exports = nextConfig
+module.exports = withBundleAnalyzer(nextConfig);
